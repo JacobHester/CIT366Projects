63bde177202bde87ed55dd16b11f2087a1351258
JacobHester <Lazjhester@gmail.com>
2018-02-08 10:07:53 -0700

    Second Part of chapter 3. My Page still refreshes when I select the send button in messages

--- cms/src/app/app.component.ts
+++ cms/src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
   loadedFeature = 'contact';
+
   onNavigate(feature: string) {
     this.loadedFeature = feature;
   }
---
+++ cms/src/app/documents/document.model.ts
@@ -0,0 +1,18 @@
+export class document {
+  public docId: string;
+  public name: string;
+  public description: string;
+  public url: string;
+  public children: string;
+
+  constructor(docId: string, name: string, description: string,
+              url: string, children: string) {
+    this.docId = docId;
+    this.name = name;
+    this.description = description;
+    this.url = url;
+    this.children = children;
+  }
+
+}
+
