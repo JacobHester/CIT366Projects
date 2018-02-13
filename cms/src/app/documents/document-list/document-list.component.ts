d7caec5fdcdde4cc969cdea440a62cc9effa2f28
JacobHester <Lazjhester@gmail.com>
2018-02-13 11:36:21 -0700

    First portion of assignment 4

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
+
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentListComponent implements OnInit {
 
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+
+  documents: Document[] = [
+    new Document('test', 'CIT 260 - Object Oriented Programming', 'Code in Java', '#', '#'),
+    new Document('test', 'CIT 366 - Full Web Stack Development', 'Learn how to develop modern web applications using the MEAN stack', 'https://content.byui.edu/file/b7c3e5ed-6947-4971-9d32-4e5b6b397cac/1/CIT 366 course description.pdf', '#'),
+    new Document('test', 'CIT 425 - Data Warehousing', 'Database Stuff', '#', '#'),
+    new Document('test', 'CIT 460 - Enterprise Development', 'Mer', '#', '#'),
+    new Document('test', 'CIT 495 - Senior Practicum', 'Yer', '#','#')
+  ];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
--- cms/src/app/documents/document.model.ts
+++ cms/src/app/documents/document.model.ts
@@ -1,4 +1,4 @@
-export class document {
+export class Document {
   public docId: string;
   public name: string;
   public description: string;
